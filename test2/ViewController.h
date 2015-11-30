6c93e903c1dc1e1f62ff72b160c906d5f7199d1b
Kanybek <kano.momuke@gmail.com>
2015-11-30 11:07:48 +0600

    Initial Commit

---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by kanybek on 11/30/15.
+//  Copyright © 2015 kanybek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
