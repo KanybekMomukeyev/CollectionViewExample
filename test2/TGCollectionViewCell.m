1a527aedc73bf24bc27474c8fe93ebc93e71ccda
Kanybek <kano.momuke@gmail.com>
2015-11-30 18:09:00 +0600

    working for one image row.

--- test2/TGCollectionViewCell.m
+++ test2/TGCollectionViewCell.m
@@ -10,11 +10,20 @@
 
 @implementation TGCollectionViewCell
 
-- (void)layoutSubviews
+- (void)awakeFromNib
 {
-    [super layoutSubviews];
-    self.image.frame = self.contentView.frame;
-    self.label.frame = self.contentView.frame;
+    [super awakeFromNib];
+    self.label.numberOfLines = 0;
+    self.label.lineBreakMode = NSLineBreakByWordWrapping;
+    [[self.image layer] setBorderWidth:2.0f];
+    [[self.image layer] setBorderColor:[UIColor greenColor].CGColor];
 }
+
+//- (void)layoutSubviews
+//{
+//    [super layoutSubviews];
+//    self.image.frame = CGRectMake(0, 0, self.contentView.bounds.size.width, 100);
+//    self.label.frame = CGRectMake(0, 100, self.contentView.bounds.size.width, self.label.frame.size.height);
+//}
 
 @end
--- test2/ViewController.m
+++ test2/ViewController.m
@@ -18,17 +18,23 @@
 #import "TGCollectionViewCell3.h"
 #import "TGCollectionViewCell4.h"
 
-#define SCREEN_WIDTH ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.width : [[UIScreen mainScreen] bounds].size.height)
-
-#define SCREEN_HEIGHT ((([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) || ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortraitUpsideDown)) ? [[UIScreen mainScreen] bounds].size.height : [[UIScreen mainScreen] bounds].size.width)
-
 @interface ViewController ()
 @property (nonatomic, strong) IBOutlet UICollectionView *collectionView;
 @property (nonatomic, strong) NSMutableArray *items;
+@property (nonatomic, strong) NSMutableArray *images;
 @property (nonatomic, strong) UICollectionViewFlowLayout *flowLayout;
 @end
 
 @implementation ViewController
+
+- (NSMutableArray *)images
+{
+    if (!_images)
+    {
+        _images = [NSMutableArray new];
+    }
+    return _images;
+}
 
 - (NSMutableArray *)items
 {
@@ -56,6 +62,7 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    [self generateDifferenImages];
     [self generateRandomCellItems];
 }
 
@@ -71,24 +78,48 @@
 }
 
 #pragma mark - Private methods
++ (CGFloat)heightForText:(NSString*)text
+                    font:(UIFont*)font
+             withinWidth:(CGFloat)width
+{
+    CGSize constraint = CGSizeMake(width, 20000.0f);
+    CGSize size;
+    CGSize boundingBox = [text boundingRectWithSize:constraint
+                                            options:NSStringDrawingUsesLineFragmentOrigin
+                                         attributes:@{NSFontAttributeName:font}
+                                            context:nil].size;
+    
+    size = CGSizeMake(ceil(boundingBox.width), ceil(boundingBox.height));
+    return size.height;
+}
+
+
+- (void)generateDifferenImages
+{
+    for (NSUInteger index = 0; index < 300; index ++)
+    {
+        [self.images addObject:[NSString stringWithFormat:@"https://placeimg.com/480/320/people/%@",@(index)]];
+    }
+}
+
 - (void)generateRandomCellItems
 {
     __block NSMutableArray *allItems = [NSMutableArray new];
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        for (NSUInteger index = 0; index < 300; index ++)
+        for (NSUInteger index = 0; index < 400; index ++)
         {
             TGMessage *message = [TGMessage new];
             NSInteger randomInteger = arc4random() % 4 + 1;
             
             NSMutableArray *images = [NSMutableArray new];
             for (NSUInteger index = 0; index < randomInteger; index ++) {
-                [images addObject:[NSString stringWithFormat:@"http://placehold.it/400x400&text=image%@",@(index)]];
+                [images addObject:[self.images objectAtIndex:(arc4random()%298 + 1)]];
             }
             message.imageItems = [[NSArray alloc] initWithArray:images];
             
-            NSString *alphabet  = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZY0123456789";
+            NSString *alphabet  = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZY0123456789 ";
             NSMutableString *s = [NSMutableString stringWithCapacity:20];
-            for (NSUInteger i = 0; i < 300; i++) {
+            for (NSUInteger i = 0; i < 500; i++) {
                 u_int32_t r = arc4random() % [alphabet length];
                 unichar c = [alphabet characterAtIndex:r];
                 [s appendFormat:@"%C", c];
@@ -109,55 +140,34 @@
     return 1;
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section
 {
     return self.items.count;
 }
 
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewCell *glCell = nil;
-    switch (arc4random() % 1) {
-        case 0:{
-            TGCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TGCollectionViewCell"
-                                                             forIndexPath:indexPath];
-            TGMessage *message = [self.items objectAtIndex:indexPath.row];
-            NSString *imageUrl = [message.imageItems lastObject];
-            [cell.image sd_setImageWithURL:[NSURL URLWithString:imageUrl]
-                          placeholderImage:[UIImage imageNamed:@"placeholder"] options:indexPath.row == 0 ? SDWebImageRefreshCached : 0];
-            cell.label.text = message.messageString;
-            glCell = cell;
-            break;
-        }
-        case 1:{
-            TGCollectionViewCell2 *cell2 = [collectionView dequeueReusableCellWithReuseIdentifier:@"TGCollectionViewCell2"
-                                                                                    forIndexPath:indexPath];
-            glCell = cell2;
-            break;
-        }
-        case 2:{
-            TGCollectionViewCell3 *cell3 = [collectionView dequeueReusableCellWithReuseIdentifier:@"TGCollectionViewCell3"
-                                                             forIndexPath:indexPath];
-            glCell = cell3;
-            break;
-        }
-        case 3:{
-            TGCollectionViewCell3 *cell4 = [collectionView dequeueReusableCellWithReuseIdentifier:@"TGCollectionViewCell4"
-                                                             forIndexPath:indexPath];
-            glCell = cell4;
-            break;
-        }
-        default:
-            break;
-    }
-    return glCell;
+    TGCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TGCollectionViewCell"
+                                                                           forIndexPath:indexPath];
+    TGMessage *message = [self.items objectAtIndex:indexPath.row];
+    NSString *imageUrl = [message.imageItems lastObject];
+    [cell.image sd_setImageWithURL:[NSURL URLWithString:imageUrl]];
+    cell.label.text = message.messageString;
+    return cell;
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout methods
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout*)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     TGMessage *message = [self.items objectAtIndex:indexPath.item];
-    return CGSizeMake(SCREEN_WIDTH - 2*10, 166.f);
+    CGFloat height = [ViewController heightForText:message.messageString
+                                              font:[UIFont systemFontOfSize:17]
+                                       withinWidth:(320 - 2*10)];
+    return CGSizeMake(320 - 2 * 10, height);
 }
 
 //- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
